5f301222f80c7642c1ef982f9fda6287a01a03f1
slientServer <brianhao211@163.com>
2017-04-16 19:34:30 +0800

    Program finished

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -2,6 +2,19 @@ module.exports= function(app) {
 	app.get('/', function(req, res){
 		res.redirect('/posts');
 	});
+	// 404 page
+	app.use(function (req, res) {
+	  if (!res.headersSent) {
+	    res.status(404).render('404');
+	  }
+	});
+
+	// error page
+	app.use(function (err, req, res, next) {
+	  res.render('error', {
+	    error: err
+	  });
+	});
 
 	app.use('/signup', require('./signup'));
 	app.use('/signin', require('./signin'));
